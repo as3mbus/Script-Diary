553085bf050ee2342c9a5892720e8177498f5d50
as3mbus <asembus@gmail.com>
2017-12-02 20:41:52 +0700

    add firefox chmod for updating

--- nightly_setup.sh
+++ nightly_setup.sh
@@ -4,4 +4,5 @@ sudo apt remove firefox
 curl -L -o nightly_latest.tar.bz2  "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64&lang=en-US"
 sudo tar -jxf nightly_latest.tar.bz2 -C /opt
 sudo ln -s /opt/firefox/firefox /usr/bin/firefox
+sudo chmod 775 -R /opt/firefox
 rm nightly_latest.tar.bz2
