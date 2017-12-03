02697ad78a3c4a70e8c13b0e1c03f5b302ce009e
as3mbus <asembus@gmail.com>
2017-12-03 11:28:51 +0700

    add unity 3d installation startup script

---
+++ unity3D-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install mono-reference-assemblies-2.0 mono-reference-assemblies-3.5
+echo 'download latest unity 3d package here [https://forum.unity3d.com/threads/unity-on-linux-release-notes-and-known-issues.350256/]'
+echo 'install it with [dpkg -i </package/path>] to make it more responsive (CLI wise)'
+echo 'and also dont forget to [sudo apt install -f] it after installation complete'
