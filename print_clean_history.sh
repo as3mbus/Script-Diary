9838862405c6e8873a7cf002bb3a26c6d8dc885a
as3mbus <asembus@gmail.com>
2017-12-04 18:39:38 +0700

    change call for zsh history since it shows time which really bad :(

--- print_clean_history.sh
+++ print_clean_history.sh
@@ -2,4 +2,4 @@
 echo "===================BASH SECTION==================="
 cat ~/.bash_history| grep -v 'git\|Telegram\|hist\|vim\|inkscape\|cd\|ls\|curl\|wget'
 echo "====================ZSH SECTION==================="
-cat ~/.zsh_history| grep -v 'git\|Telegram\|hist\|vim\|inkscape\|cd\|ls\|curl\|wget'
+history| grep -v 'git\|Telegram\|hist\|vim\|inkscape\|cd\|ls\|curl\|wget'|grep sudo
