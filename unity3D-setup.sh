bc20a231e60a53b0f86affd5d81fcb755cb1e7fe
as3mbus <asembus@gmail.com>
2017-12-04 18:40:06 +0700

    remove mono reference since upgrade would remove it as well

--- unity3D-setup.sh
+++ unity3D-setup.sh
@@ -2,7 +2,7 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
 sudo apt-get update
-sudo apt install monodevelop mono-reference-assemblies-2.0 mono-reference-assemblies-3.5
+sudo apt install monodevelop 
 
 echo 'download latest unity 3d package here [https://forum.unity3d.com/threads/unity-on-linux-release-notes-and-known-issues.350256/]'
 echo 'install it with [dpkg -i </package/path>] to make it more responsive (CLI wise)'
