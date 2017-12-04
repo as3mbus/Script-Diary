2a8346b4d701da8b186b11b6bcab848f4b8ef960
as3mbus <asembus@gmail.com>
2017-12-04 18:41:51 +0700

    add nvidia proprietary driver setup (not tested yet)

---
+++ nvidia_driver_setup.sh
@@ -0,0 +1,15 @@
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt install nvidia-prime nvidia-384
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
+sudo apt install bumblebee bumblebee-nvidia primus bbswitch-dkms
+sudo prime-select intel
+sudo apt upgrade
+curl -L -o prime-indicator-master.zip  "https://github.com/andrebrait/prime-indicator/archive/master.zip"
+unzip prime-indicator-master.zip
+cd prime-indicator-master
+sudo chmod +x setup.sh
+sudo ./setup.sh
+curl -L -o small-icons.zip  "https://github.com/andrebrait/prime-indicator/files/591779/small-icons.zip"
+unzip small-icons.zip -d /usr/lib/prime-indicator/icons
