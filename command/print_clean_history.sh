c40f58561065f6ae9c9a97aa3fe7ad3c57ba663f
as3mbus <asembus@gmail.com>
2017-12-04 20:12:21 +0700

    remove extension for command

--- command/print_clean_history.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh -i
-echo "===================BASH SECTION==================="
-cat ~/.bash_history| grep -v 'git\|Telegram\|hist\|vim\|inkscape\|cd\|ls\|curl\|wget'
-echo "====================ZSH SECTION==================="
-history| grep -v 'git\|Telegram\|hist\|vim\|inkscape\|cd\|ls\|curl\|wget'|grep sudo
